4c8cc881aea718cc90b34e5ba3f44a50b7f896a8
Hubert OG <hubert@orlikarnia.com>
2013-02-27 00:28:35 +0100

    Compile error version

--- public/-/m/blog/list/list.html/template.js
+++
@@ -1,2 +0,0 @@
-Meteor._def_template("im-blog-list",Handlebars.json_ast_to_func([["#",[[0,"each"],[0,"documents"]],["\n    <div>",["{",[[0,"name"]]],"</div>\n  "]]]));
-Meteor._def_template("im-blog-listEntry",Handlebars.json_ast_to_func(["<div>\n    document:\n    ",["{",[[0,"header"]]],"\n  </div>"]));
--- public/-/m/blog/main.js
+++
@@ -1,7 +0,0 @@
-require(['/-/m/blog/_/_.js'],function(){
-require(['/-/m/blog/list/list._template.js'],function(){
-require(['/-/m/blog/list/list.html/template.js'],function(){
-require(['/-/m/blog/list/list.js'],function(){
-console.log('MODULE LOADED: [blog]');
-Impact.loadedModuleConstructor('blog');
-});});});});
