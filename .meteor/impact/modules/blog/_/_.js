41be12673c10f9c475b6b41d25cbe523caf28afc
Hubert OG <hubert@orlikarnia.com>
2013-02-28 00:57:00 +0100

    Some small stuff for future tweaks

--- .meteor/impact/modules/blog/_/_.js
+++ .meteor/impact/modules/blog/_/_.js
@@ -1,7 +1,27 @@
 
 exports.render = function(state) {
-  console.log("RENDERING BLOG");
-  return "<div>TAK WYGLĄDA BLOG!</div>";
+  //#  /                      -> list
+  //#  /articleTitle/id       -> show
+  //#  /articleTitle/id/edit  -> edit
+
+  var template = Impact.mrouter(state.getPath(), {
+    '/':                  'list',
+    '/:title/:id':        function(title, id) {
+                            S('articleId', id);
+                            return 'show';
+                          },
+    '/:title/:id/edit':   function(title, id) {
+                            S('articleId', id);
+                            return 'edit';
+                          }
+  });
+
+  return Template[template]();
+  // if(state.getPath().length == 0) {
+
+  // } else if(state.getPath()[0] == )
+  // console.log("RENDERING BLOG");
+  // return "<div>TAK WYGLĄDA BLOG!</div>";
 };
 
 console.log("LOADED BLOG _");
