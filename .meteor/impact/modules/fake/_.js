0208966dc21edb35c41a6f017a0be5196bf13caa
Tomasz Lenarcik <tomek.lenarcik@gmail.com>
2013-02-28 01:13:38 +0100

    small bugfix
    in loader function (module factory) we need Template instead of
    Templates

--- .meteor/impact/modules/fake/_.js
+++ .meteor/impact/modules/fake/_.js
@@ -1,7 +1,7 @@
 
 
 exports.render = function (state) {
-  return 'LOL';
+  return Template.base();
  /* var path   = state.getPath();
   var params = state.getParams();
   console.log('renderd with state', path, params.fetch());*/
