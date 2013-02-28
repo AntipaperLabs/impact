cd58273a6135ae7a52351690bdd184837e57eb02
Tomasz Lenarcik <tomek.lenarcik@gmail.com>
2013-02-28 01:03:11 +0100

    messing arround with some tests

--- .meteor/impact/modules/fake/_.js
+++ .meteor/impact/modules/fake/_.js
@@ -1,12 +1,8 @@
 
-var fake = function () {};
 
-var Fake = fake.prototype;
-
-Fake.render = function (state) {
-  var path   = state.getPath();
+exports.render = function (state) {
+  return 'LOL';
+ /* var path   = state.getPath();
   var params = state.getParams();
-  console.log('renderd with state', path, params.fetch());
+  console.log('renderd with state', path, params.fetch());*/
 };
-
-Impact.has.module('fake', fake);
