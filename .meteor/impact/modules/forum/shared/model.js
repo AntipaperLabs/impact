963282304031c039fd077f0836cfd8091c32b871
Hubert OG <hubert@orlikarnia.com>
2013-03-04 08:32:34 +0100

    Repacked modules using .dot feature.

--- .meteor/impact/modules/forum/shared/model.js
+++
@@ -1,6 +0,0 @@
-var Areas =     Documents.subcollection('areas');
-var Threads =   Documents.subcollection('threads');
-var Posts =     Documents.subcollection('posts');
-
-var Stars =         Notes.subcollection('stars');
-var Subscriptions = Notes.subcollection('subscriptions');
