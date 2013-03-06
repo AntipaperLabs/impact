1f66c51740799eef8b7bcfa6419f9c37ee572856
Hubert OG <hubert@orlikarnia.com>
2013-03-06 11:34:51 +0100

    Prepared for simplification

--- client/yield/module_helpers.js
+++ client/yield/module_helpers.js
@@ -3,35 +3,11 @@
 ////////////////////
 
 
-// Handlebars.registerHelper('renderModule',
+
 Template.yield.helpers({
   renderModule: function() {
 
-    var state  = Impact.Yield;
-    var moduleName = Impact.Yield.getCurrentModule();
 
-    var module = Impact.ModuleManager.getInstance(moduleName);
-
-    // TODO-T
-    // Without moduleName check dashboard will not render.
-    // Still, it seems very fiddly. This function needs to update reactively
-    // in order for paths put earlier in the router to work.
-    // Is there another way?
-    if (moduleName && module) {
-        return new Handlebars.SafeString(module.render(state) || '');
-    }
-
-    console.log(moduleName);
-
-    // EXTRACTED to upper level
-    // probably remove
-    //
-    // if renderer is undefined fallback to standard router behavior
-    // var name = Meteor.Router.page();
-    // var template = Template[name];
-    // if (template) {
-    //   return new Handlebars.SafeString(template());
-    // }
 
     return 'ERROR: UNKNOWN MODULE';
   },
