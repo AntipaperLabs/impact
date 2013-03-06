e73501b3d70efd6dc40692539197a65d17c20493
apendua <tomek.lenarcik@gmail.com>
2013-03-06 22:20:56 +0100

    deffered the instance creation process

--- client/yield/instance.js
+++ client/yield/instance.js
@@ -3,7 +3,12 @@
   Impact.ModuleInstance = function (moduleName, options) {
     options = options || {};
     this._impact = options;
-    this._impact.moduleName = moduleName;
+    this._impact.moduleName = moduleName || options.moduleName;
+
+    // if we want to skip registration process and set moduleName
+    // properly we can pass module name using options hash
+    if (moduleName)
+      Impact.ModuleManager.registerInstance (moduleName, this);
   };
 
   $functions(Impact.ModuleInstance, {
