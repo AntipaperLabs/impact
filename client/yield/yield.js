917e03ec9b4a5f8d04236c7fe377df3fa470b15d
Hubert OG <hubert@orlikarnia.com>
2013-03-08 04:18:57 +0100

    Router works!

--- client/yield/yield.js
+++ client/yield/yield.js
@@ -114,11 +114,12 @@
       return this.params;
     },
 
-    setCurrentModuleAndState: function (moduleName, path, params) {
+    setCurrentModuleAndState: function (moduleName, path, params, fullPath) {
       // if (this.currentModule === currentModule) {
         // return; // do not invalidate
       this.moduleName = moduleName;
       this.path = path;
+      this.fullPath = fullPath;
       this.getParams().copy(params);
       this._invalidate();
     },
@@ -139,25 +140,43 @@
       var name = path[0];
       path.splice(0, 1);
       var params = $.deparam(context.querystring || '');
-      this.setCurrentModuleAndState(name, path, params);
+      this.setCurrentModuleAndState(name, path, params, fullPath);
     },
 
 
     matchRoute: function(map) {
       var self = this;
-
+      console.log("MATCHING ROUTE ["+this.fullPath+"]");
+      console.log("OR BETTER: ", this.path);
 
       for(var key in map) {
-        // console.log("MROUTER test key:", key);
-        // if(!Impact.mrouter.match(array, key)) continue;
+        if(!this._routeMatches(this.path, key)) continue;
+
         var value = map[key];
-        // console.log("MROUTER match found!", value);
+        console.log("MROUTER match found!", value);
+
         if(typeof value == "string") return value;
-        // if(typeof value == "function") return value.apply(this, array);
+        if(typeof value == "function") return value.apply(this, this.path);
         continue;
       }
 
       return undefined;
+    },
+
+    _routeMatches: function(array, string) {
+      if(string.startsWith('/')) string = string.substring(1);
+      var tab = string.split('/');  
+      if(tab[0] == "") {
+        tab.removeAt(0);
+      }
+
+      if(array.length != tab.length) return false;
+      for(var i = 0; i < tab.length; ++i) {
+        if(tab[i] == '?') continue;
+        if(tab[i].startsWith(':')) continue;
+        if(array[i] != tab[i]) return false;
+      }
+      return true;
     },
 
     
