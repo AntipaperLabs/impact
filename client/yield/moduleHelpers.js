844a40dc91eeda3a6441082c6b1ca6f834951d4c
Hubert OG <hubert@orlikarnia.com>
2013-03-08 04:58:39 +0100

    Probably fixed post merge problems.

--- client/yield/moduleHelpers.js
+++ client/yield/moduleHelpers.js
@@ -7,7 +7,7 @@
 Template.yield.helpers({
   renderModule: function() {
 
-    var state  = Impact.Yield;
+    var state = Impact.Yield;
     var moduleName = Impact.Yield.getCurrentModule();
 
     var module = Impact.ModuleManager.getInstance(moduleName);
@@ -18,10 +18,20 @@ Template.yield.helpers({
     // in order for paths put earlier in the router to work.
     // Is there another way?
     if (moduleName && module) {
+        // console.log("HLP: MODULE EXISTS. ", module);
+        // console.log("HLP: CURRENT STATE: ", state);
+        if(module.routes) {
+          var template = module.routes(state);
+          console.log("ROUTED TO IN-MODULE TEMPLATE ["+template+"]");
+          if(template)
+            return new Handlebars.SafeString(Template['im1-'+moduleName+'-'+template]());
+          else
+            return "WRONG TEMPLATE FOUND";
+        }
         return new Handlebars.SafeString(module.render(state) || '');
-    }
+      }
 
-    console.log(moduleName);
+      console.log(moduleName);
 
     // EXTRACTED to upper level
     // probably remove
@@ -30,12 +40,15 @@ Template.yield.helpers({
     // var name = Meteor.Router.page();
     // var template = Template[name];
     // if (template) {
-    //   return new Handlebars.SafeString(template());
+    // return new Handlebars.SafeString(template());
     // }
 
     return 'ERROR: UNKNOWN MODULE';
   },
 });
+
+
+
 
 
 ////////////////////
