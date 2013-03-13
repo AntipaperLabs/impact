7cdceb62a8ea79865ceb1290caa37bbb64505694
Tomasz Lenarcik <tomek.lenarcik@gmail.com>
2013-03-13 21:41:13 +0100

    adding fake article for testing

--- .impact/modules/blog/client/list.js
+++ .impact/modules/blog/client/list.js
@@ -2,3 +2,21 @@
 Template.list.articles = function() {
   return Articles.find({});
 }
+
+Template.list.events({
+  'click input[value=add]': function () {
+    Articles.insert({
+      title : 'some math article',
+      body  : [
+        {
+          type: 'text',
+          content: 'this is article with math content',
+        },
+        {
+          type: 'math',
+          content: 'some math content: $x^2+y^2=z^2$',
+        },
+      ]
+    });
+  }
+});
